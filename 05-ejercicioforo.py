ec525a359c6f72cb23d536bdfd7cf546fc953726
Yadi2308 <yadi2308@gmail.com>
2024-07-15 17:38:49 -0500

    Add files via upload

---
+++ 05-ejercicioforo.py
@@ -0,0 +1,4 @@
+print("Creacion de nombre para cerveza")
+pregunta1 = input("Escriba la primera silaba de su nombre: ")
+pregunta2 = input("Escriba la primera silaba de su apellido: ")
+print(("El nombre para la cerveza es: ") + pregunta1 + pregunta2)
