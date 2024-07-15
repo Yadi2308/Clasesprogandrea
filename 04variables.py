056d92c7fe50adc3b8053136845e13d74ee98771
Yadi2308 <yadi2308@gmail.com>
2024-07-15 16:54:11 -0500

    Add files via upload

---
+++ 04variables.py
@@ -0,0 +1,24 @@
+#nombre = "andrea"
+#print(nombre)
+
+#edad = "30"
+#print(edad)
+
+#edad1 = 30
+#edad2 = 20
+#print(edad1 + edad2)
+#nombre = input("dime tu nombre: ")
+#print("tu nombre es:" + nombre)
+nombre = "Tony Soprano"
+edad = 51
+print(nombre)
+print(edad)
+
+nombre = "Julia"
+apellido = "Roberts"
+nombre_completo = nombre + " " + apellido
+print("el nombre es: " + nombre_completo)
+
+curso = "python"
+print("estas tomando un curso de: "+ curso)
+
